9e017804b16cc9e7c3dc243207bdfe58ccbbed35
marco <dobermannpoker86@gmail.com>
2020-04-17 13:55:17 +0200

    scheletro fatto

---
+++ my.js
@@ -0,0 +1,23 @@
+
+// DESCRIZIONE:
+// Griglia 6x6,
+// ad ogni click (su ogni rettangolino) parte una richiesta AJAX che prende un numero random da 1 a 9 (primo end-point della API in slide).
+// Se il num ritornato è <= 5 il quadrato diventa giallo,
+// se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato
+$(document).ready(function() {
+$.ajax({
+    url : "https://flynn.boolean.careers/exercises/api/random/int",
+    method : "GET",
+    success : function (data,stato) {
+        console.log(data.response , data.success);
+        $("h1").text(data.response);
+    },
+    error : function (richiesta,stato,errori) {
+        console.log("e un errore. " + errori, "stato " + stato, richiesta  );
+    }
+})
+
+
+// fine
+});
